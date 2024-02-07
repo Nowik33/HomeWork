6499ce1be005bcd648cb5835abbc003cd157b433
Nowik33 <157578532+Nowik33@users.noreply.github.com>
2024-02-07 16:31:15 +0300

    Add files via upload

---
+++ dz_240131/dz_240131.cpp
@@ -0,0 +1,49 @@
+﻿// dz_240131.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+/*
+Напишите класс - конвертер валюты.
+Умеет работать с RUB DOL EUR
+Берем текущий курс
+*/
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+class Converter 
+{
+    double rub;
+    double usd;
+    double eur;
+public:
+    Converter (double rub, double usd,double eur) 
+    {
+        this->rub = rub;
+        this->usd = usd;
+        this->eur = eur;
+    }
+    void show()
+    {
+        cout << "===============================" << endl;
+        cout << "сумма USD : " << fixed << setprecision(2) << rub / usd << endl;
+        cout << "сумма EUR : " << fixed << setprecision(2) << rub / eur << endl;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL,"");
+    double vusd = 0;
+    double veur = 0;
+    double vrub = 0;
+    cout << "ВВОД КУРСОВЫХ ЗНАЧЕНИЙ USD, EUR" << endl;
+    cout << "НА СЕГОДНЯ ПО ДАННЫМ ЦБ РФ" << endl; 
+    cout << "===============================" << endl;
+    cout << "Курс USD : ";
+    cin >> vusd;
+    cout << "Курс EUR : ";
+    cin >> veur;
+    cout << "Сумма RUB : ";
+    cin >> vrub;
+    Converter cv(vrub, vusd, veur);
+    cv.show();
+}
+
